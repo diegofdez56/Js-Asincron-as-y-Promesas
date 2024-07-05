a48a4223db9bd5ec05213431b46cd7b1a1f11997
diegoals13 <diegoalnso13@gmail.com>
2024-07-05 17:53:11 +0200

    Added Async.js

---
+++ public/js/async.js
@@ -0,0 +1,10 @@
+
+export async function fetchTodos() {
+    try {
+        const response = await fetch('data.json');
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        console.error('Error fetching todos:', error);
+    }
+}
